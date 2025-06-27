73c2a0e5c3fb8561364a5abc6348267716179b34
notokeisho <12012tok73@gmail.com>
2025-06-27 07:19:10 +0000

    sportsテーブルの作成

--- api/db_schema/migrations/20250616065704_add_sports_domain.sql
+++ api/db_schema/migrations/20250616065704_add_sports_domain.sql
@@ -4,11 +4,7 @@ CREATE TABLE `sports`
     id      VARCHAR(26)  NOT NULL COMMENT 'ID',
     name    VARCHAR(64)  NOT NULL COMMENT 'スポーツ名',
     weight  INT UNSIGNED  NOT NULL COMMENT '表示順',
-    icon_id VARCHAR(26)  NOT NULL COMMENT 'アイコンID',
-    rule_id VARCHAR(26)  NOT NULL COMMENT 'ルールID',
-    PRIMARY KEY (id),
-    FOREIGN KEY (icon_id) REFERENCES icons(id) ON DELETE CASCADE,
-    FOREIGN KEY (rule_id) REFERENCES rules(id) ON DELETE CASCADE
+    PRIMARY KEY (id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_bin;
