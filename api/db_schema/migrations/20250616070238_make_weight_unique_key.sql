572e2aa88c7093b50317126203cf38a1a2e6eca9
notokeisho <12012tok73@gmail.com>
2025-07-03 16:21:22 +0000

    UKとAUTOINCREMENTの削除

--- api/db_schema/migrations/20250616070238_make_weight_unique_key.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE sports ADD CONSTRAINT unique_weight UNIQUE (weight);
-
--- migrate:down
-ALTER TABLE sports DROP CONSTRAINT unique_weight;
--- api/db_schema/migrations/20250628072052_add_weight_autoincrement.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE sports MODIFY weight INT NOT NULL AUTO_INCREMENT COMMENT '表示順';
-
--- migrate:down
-ALTER TABLE sports MODIFY weight INT NOT NULL COMMENT '表示順';
