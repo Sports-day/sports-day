ce60041cf9e747cf5d2e019ae5963d78a6310435
ReTic0310 <retic0310@gmail.com>
2025-07-12 07:26:09 +0000

    feat: 大会機能用のFormatterを追加

--- api/graph/model/format_response.go
+++ api/graph/model/format_response.go
@@ -33,3 +33,10 @@ func FormatLocationResponse(location *db_model.Location) *Location {
 		Name: location.Name,
 	}
 }
+
+func FormatCompetitionResponse(competition *db_model.Competition) *Competition {
+	return &Competition{
+		ID:   competition.ID,
+		Name: competition.Name,
+	}
+}
