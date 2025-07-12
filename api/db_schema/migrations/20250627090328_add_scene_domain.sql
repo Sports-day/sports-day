59fad83eafff7a85a238edde385cd605ee40eac2
hakase61912 <hakase61912@icloud.com>
2025-07-12 02:07:18 +0000

    change: テーブルの表記をscenesに変更しました

--- api/db_schema/migrations/20250627090328_add_scene_domain.sql
+++ api/db_schema/migrations/20250627090328_add_scene_domain.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-CREATE TABLE `scene` (
+CREATE TABLE `scenes` (
     id   VARCHAR(26) NOT NULL COMMENT 'ID',
     name VARCHAR(64) NOT NULL COMMENT 'シーン名',
     created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -11,4 +11,4 @@ COLLATE = utf8mb4_bin;
 
 -- migrate:down
 
-DROP TABLE `scene`;
+DROP TABLE `scenes`;
