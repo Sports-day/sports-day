f797ae40db691cc27edcce624dde8917f5ad1372
27819game <27819game@gmail.com>
2025-07-13 05:28:22 +0000

    feat: informationのformatを定義しました

--- api/graph/model/format_response.go
+++ api/graph/model/format_response.go
@@ -18,3 +18,11 @@ func FormatGroupResponse(group *db_model.Group) *Group {
 		Name: group.Name,
 	}
 }
+
+func FormatInformationResponse(information *db_model.Information) *Information {
+	return &Information{
+		ID:      information.ID,
+		Title:   information.Title,
+		Content: information.Content,
+	}
+}
