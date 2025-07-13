122426e171f33fe812d559fd37b0266fa60ae374
ReTic0310 <retic0310@gmail.com>
2025-07-13 16:20:20 +0000

    feat: 試合/場所機能のローダーの定義

--- api/loader/loader.go
+++ api/loader/loader.go
@@ -10,32 +10,44 @@ import (
 )
 
 type Loaders struct {
-	UserLoader             *dataloadgen.Loader[string, *db_model.User]
-	GroupLoader            *dataloadgen.Loader[string, *db_model.Group]
-	TeamLoader             *dataloadgen.Loader[string, *db_model.Team]
-	CompetitionLoader      *dataloadgen.Loader[string, *db_model.Competition]
-	GroupUsersLoader       *dataloadgen.Loader[string, []*db_model.GroupUser]
-	UserGroupsLoader       *dataloadgen.Loader[string, []*db_model.GroupUser]
-	TeamUsersLoader        *dataloadgen.Loader[string, []*db_model.TeamUser]
-	UserTeamsLoader        *dataloadgen.Loader[string, []*db_model.TeamUser]
-	GroupTeamsLoader       *dataloadgen.Loader[string, []*db_model.Team]
+	UserLoader               *dataloadgen.Loader[string, *db_model.User]
+	GroupLoader              *dataloadgen.Loader[string, *db_model.Group]
+	TeamLoader               *dataloadgen.Loader[string, *db_model.Team]
+	CompetitionLoader        *dataloadgen.Loader[string, *db_model.Competition]
+	LocationLoader           *dataloadgen.Loader[string, *db_model.Location]
+	MatchLoader              *dataloadgen.Loader[string, *db_model.Match]
+	GroupUsersLoader         *dataloadgen.Loader[string, []*db_model.GroupUser]
+	UserGroupsLoader         *dataloadgen.Loader[string, []*db_model.GroupUser]
+	TeamUsersLoader          *dataloadgen.Loader[string, []*db_model.TeamUser]
+	UserTeamsLoader          *dataloadgen.Loader[string, []*db_model.TeamUser]
+	GroupTeamsLoader         *dataloadgen.Loader[string, []*db_model.Team]
 	CompetitionEntriesLoader *dataloadgen.Loader[string, []*db_model.CompetitionEntry]
-	EntryCompetitionsLoader *dataloadgen.Loader[string, []*db_model.CompetitionEntry]
+	EntryCompetitionsLoader  *dataloadgen.Loader[string, []*db_model.CompetitionEntry]
+	MatchEntriesLoader       *dataloadgen.Loader[string, []*db_model.MatchEntry]
+	EntryMatchesLoader       *dataloadgen.Loader[string, []*db_model.MatchEntry]
+	CompetitionMatchesLoader *dataloadgen.Loader[string, []*db_model.Match]
+	LocationMatchesLoader    *dataloadgen.Loader[string, []*db_model.Match]
 }
 
-func New(userSvc service.User, groupSvc service.Group, teamSvc service.Team, competitionSvc service.Competition) *Loaders {
+func New(userSvc service.User, groupSvc service.Group, teamSvc service.Team, competitionSvc service.Competition, locationSvc service.Location, matchSvc service.Match) *Loaders {
 	return &Loaders{
-		UserLoader:             dataloadgen.NewLoader(newUserLoader(userSvc)),
-		GroupLoader:            dataloadgen.NewLoader(newGroupLoader(groupSvc)),
-		TeamLoader:             dataloadgen.NewLoader(newTeamLoader(teamSvc)),
-		CompetitionLoader:      dataloadgen.NewLoader(newCompetitionLoader(competitionSvc)),
-		GroupUsersLoader:       dataloadgen.NewLoader(newGroupUsersLoader(groupSvc)),
-		UserGroupsLoader:       dataloadgen.NewLoader(newUserGroupsLoader(groupSvc)),
-		TeamUsersLoader:        dataloadgen.NewLoader(newTeamUsersLoader(teamSvc)),
-		UserTeamsLoader:        dataloadgen.NewLoader(newUserTeamsLoader(teamSvc)),
-		GroupTeamsLoader:       dataloadgen.NewLoader(newGroupTeamsLoader(teamSvc)),
+		UserLoader:               dataloadgen.NewLoader(newUserLoader(userSvc)),
+		GroupLoader:              dataloadgen.NewLoader(newGroupLoader(groupSvc)),
+		TeamLoader:               dataloadgen.NewLoader(newTeamLoader(teamSvc)),
+		CompetitionLoader:        dataloadgen.NewLoader(newCompetitionLoader(competitionSvc)),
+		LocationLoader:           dataloadgen.NewLoader(newLocationLoader(locationSvc)),
+		MatchLoader:              dataloadgen.NewLoader(newMatchLoader(matchSvc)),
+		GroupUsersLoader:         dataloadgen.NewLoader(newGroupUsersLoader(groupSvc)),
+		UserGroupsLoader:         dataloadgen.NewLoader(newUserGroupsLoader(groupSvc)),
+		TeamUsersLoader:          dataloadgen.NewLoader(newTeamUsersLoader(teamSvc)),
+		UserTeamsLoader:          dataloadgen.NewLoader(newUserTeamsLoader(teamSvc)),
+		GroupTeamsLoader:         dataloadgen.NewLoader(newGroupTeamsLoader(teamSvc)),
 		CompetitionEntriesLoader: dataloadgen.NewLoader(newCompetitionEntriesLoader(competitionSvc)),
-		EntryCompetitionsLoader: dataloadgen.NewLoader(newEntryCompetitionsLoader(competitionSvc)),
+		EntryCompetitionsLoader:  dataloadgen.NewLoader(newEntryCompetitionsLoader(competitionSvc)),
+		MatchEntriesLoader:       dataloadgen.NewLoader(newMatchEntriesLoader(matchSvc)),
+		EntryMatchesLoader:       dataloadgen.NewLoader(newEntryMatchesLoader(matchSvc)),
+		CompetitionMatchesLoader: dataloadgen.NewLoader(newCompetitionMatchesLoader(matchSvc)),
+		LocationMatchesLoader:    dataloadgen.NewLoader(newLocationMatchesLoader(matchSvc)),
 	}
 }
 
