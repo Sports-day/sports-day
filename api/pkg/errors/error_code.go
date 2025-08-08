b8a94052283aaff80504ac7fb2ea33e8a9ac01a8
ReTic0310 <retic0310@gmail.com>
2025-08-08 05:54:39 +0000

    change: standings用のエラーを追加

--- api/pkg/errors/error_code.go
+++ api/pkg/errors/error_code.go
@@ -41,6 +41,7 @@ var (
 	ErrUpsertLeague           = NewError("LEAGUE_UPSERT_FAILED", "リーグ情報の更新に失敗しました")
 	ErrMakeLeagueMatches      = NewError("LEAGUE_MATCH_MAKE_FAILED", "リーグの試合生成に失敗しました")
 	ErrSaveLeague             = NewError("LEAGUE_SAVE_FAILED", "リーグの保存に失敗しました")
+	ErrSaveLeagueStanding     = NewError("LEAGUE_STANDING_SAVE_FAILED", "リーグ結果の保存に失敗しました")
 	/*
 		Authentication
 	*/
