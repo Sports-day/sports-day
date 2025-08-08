566a1dea42fc7ee040ed76d70cf0d7456123d2a1
Tetsuya Yamamoto <testusuke@gmail.com>
2025-08-08 13:47:56 +0900

    style: lint

--- api/service/judgment.go
+++ api/service/judgment.go
@@ -54,7 +54,7 @@ func (s *Judgment) Update(ctx context.Context, id string, input model.UpdateJudg
 		}
 		if e.Name != nil {  
     		count++  
-}  
+		}  
 
 		// 1つ以下の場合のみ処理を続行（0個も許可、2個以上はエラー）
 		if count <= 1 {
