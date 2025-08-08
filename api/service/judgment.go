710f99cc283754168b3c0075341573711f73fe9e
ReTic0310 <retic0310@gmail.com>
2025-08-08 03:08:36 +0000

    change: バリデーションの修正

--- api/service/judgment.go
+++ api/service/judgment.go
@@ -52,14 +52,14 @@ func (s *Judgment) Update(ctx context.Context, id string, input model.UpdateJudg
 		if e.GroupID != nil {
 			count++
 		}
+		if e.Name != nil {  
+    		count++  
+}  
 
 		// 1つ以下の場合のみ処理を続行（0個も許可、2個以上はエラー）
 		if count <= 1 {
 			// nameはJudgmentEntryから取得
-			if e.Name != nil {
-				judgment.Name = pkggorm.ToNullString(e.Name)
-			}
-
+			judgment.Name = pkggorm.ToNullString(e.Name)
 			judgment.UserID = pkggorm.ToNullString(e.UserID)
 			judgment.TeamID = pkggorm.ToNullString(e.TeamID)
 			judgment.GroupID = pkggorm.ToNullString(e.GroupID)
