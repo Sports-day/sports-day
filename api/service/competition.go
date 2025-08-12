2fa577022b009d8547b7f55d18daac84f49e6a94
ReTic0310 <retic0310@gmail.com>
2025-08-12 07:16:08 +0000

    change: リーグ追加ロジックの実装

--- api/service/competition.go
+++ api/service/competition.go
@@ -16,13 +16,15 @@ type Competition struct {
 	db                    *gorm.DB
 	competitionRepository repository.Competition
 	teamRepository        repository.Team
+	leagueRepository      repository.League
 }
 
-func NewCompetition(db *gorm.DB, competitionRepository repository.Competition, teamRepository repository.Team) Competition {
+func NewCompetition(db *gorm.DB, competitionRepository repository.Competition, teamRepository repository.Team, leagueRepository repository.League) Competition {
 	return Competition{
 		db:                    db,
 		competitionRepository: competitionRepository,
 		teamRepository:        teamRepository,
+		leagueRepository:      leagueRepository,
 	}
 }
 
@@ -30,7 +32,7 @@ func (s *Competition) Create(ctx context.Context, input *model.CreateCompetition
 	competition := &db_model.Competition{
 		ID:   ulid.Make(),
 		Name: input.Name,
-		Type: string(input.Type),
+		Type: "TOURNAMENT",
 	}
 
 	competition, err := s.competitionRepository.Save(ctx, s.db, competition)
@@ -58,10 +60,6 @@ func (s *Competition) Update(ctx context.Context, id string, input model.UpdateC
 		competition.Name = *input.Name
 	}
 
-	if input.Type != nil {
-		competition.Type = string(*input.Type)
-	}
-
 	competition, err = s.competitionRepository.Save(ctx, s.db, competition)
 	if err != nil {
 		return nil, errors.ErrSaveCompetition
@@ -86,21 +84,38 @@ func (s *Competition) List(ctx context.Context) ([]*db_model.Competition, error)
 }
 
 func (s *Competition) AddEntries(ctx context.Context, competitionId string, teamIds []string) (*db_model.Competition, error) {
-	_, err := s.competitionRepository.Get(ctx, s.db, competitionId)
+	competition, err := s.competitionRepository.Get(ctx, s.db, competitionId)
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errors.ErrCompetitionNotFound
 		}
+		return nil, errors.Wrap(err)
 	}
 
-	if _, err := s.competitionRepository.AddCompetitionEntries(ctx, s.db, competitionId, teamIds); err != nil {
+	err = s.db.Transaction(func(tx *gorm.DB) error {
+		if _, err := s.competitionRepository.AddCompetitionEntries(ctx, tx, competitionId, teamIds); err != nil {
+			return err
+		}
+
+		if competition.Type == "LEAGUE" {
+			for _, teamId := range teamIds {
+				standing := &db_model.LeagueStanding{
+					ID:     competitionId,
+					TeamID: teamId,
+				}
+				if _, err := s.leagueRepository.SaveStanding(ctx, tx, standing); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
 		return nil, errors.ErrAddCompetitionEntry
 	}
 
-	competition, err := s.competitionRepository.Get(ctx, s.db, competitionId)
-	if err != nil {
-		return nil, errors.Wrap(err)
-	}
 	return competition, nil
 }
 
