7ddc91d96ccbc1dad75ca5d462423a37ed491734
ReTic0310 <retic0310@gmail.com>
2025-08-13 04:26:29 +0000

    change: リゾルバの警告文を解消

--- api/graph/model.resolvers.go
+++ api/graph/model.resolvers.go
@@ -526,21 +526,3 @@ type matchResolver struct{ *Resolver }
 type standingResolver struct{ *Resolver }
 type teamResolver struct{ *Resolver }
 type userResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *standingResolver) Gf(ctx context.Context, obj *model.Standing) (int32, error) {
-	panic(fmt.Errorf("not implemented: Gf - gf"))
-}
-func (r *standingResolver) Ga(ctx context.Context, obj *model.Standing) (int32, error) {
-	panic(fmt.Errorf("not implemented: Ga - ga"))
-}
-func (r *standingResolver) Gd(ctx context.Context, obj *model.Standing) (int32, error) {
-	panic(fmt.Errorf("not implemented: Gd - gd"))
-}
-*/
