9c5fa564d121b7e2b80edaab11a65a8c8b985ea8
hakase61912 <hakase61912@icloud.com>
2026-03-16 18:09:50 +0000

    feature: ImageServiceを追加

---
+++ api/service/image.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+
+	"sports-day/api/db_model"
+	"sports-day/api/pkg/errors"
+	"sports-day/api/pkg/ulid"
+	"sports-day/api/repository"
+
+	"gorm.io/gorm"
+)
+
+type Image struct {
+	db              *gorm.DB
+	imageRepository repository.Image
+}
+
+func NewImage(db *gorm.DB, imageRepository repository.Image) Image {
+	return Image{
+		db:              db,
+		imageRepository: imageRepository,
+	}
+}
+
+func (s *Image) CreateUploadURL(
+	ctx context.Context,
+	ownerType string,
+	ownerID *string,
+	filename string,
+) (*db_model.Image, error) {
+
+	img := &db_model.Image{
+		ID:     ulid.Make(),
+		Status: "pending",
+	}
+
+	img, err := s.imageRepository.Create(ctx, s.db, img)
+	if err != nil {
+		return nil, errors.Wrap(err)
+	}
+
+	return img, nil
+}
