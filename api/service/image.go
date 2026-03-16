40c230816cfb57e517f782e6446b5db03e9f8862
hakase61912 <hakase61912@icloud.com>
2026-03-16 21:17:50 +0000

    feature: 画像upload用ImageServiceを実装

--- api/service/image.go
+++ api/service/image.go
@@ -5,21 +5,33 @@ import (
 
 	"sports-day/api/db_model"
 	"sports-day/api/pkg/errors"
+	"sports-day/api/pkg/storage"
 	"sports-day/api/pkg/ulid"
 	"sports-day/api/repository"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"gorm.io/gorm"
 )
 
 type Image struct {
 	db              *gorm.DB
 	imageRepository repository.Image
+	s3              *s3.Client
+	bucket          string
 }
 
-func NewImage(db *gorm.DB, imageRepository repository.Image) Image {
+func NewImage(
+	db *gorm.DB,
+	imageRepository repository.Image,
+	s3 *s3.Client,
+	bucket string,
+) Image {
+
 	return Image{
 		db:              db,
 		imageRepository: imageRepository,
+		s3:              s3,
+		bucket:          bucket,
 	}
 }
 
@@ -28,7 +40,7 @@ func (s *Image) CreateUploadURL(
 	ownerType string,
 	ownerID *string,
 	filename string,
-) (*db_model.Image, error) {
+) (*db_model.Image, string, error) {
 
 	img := &db_model.Image{
 		ID:     ulid.Make(),
@@ -37,12 +49,21 @@ func (s *Image) CreateUploadURL(
 
 	img, err := s.imageRepository.Create(ctx, s.db, img)
 	if err != nil {
-		return nil, errors.Wrap(err)
+		return nil, "", errors.Wrap(err)
 	}
 
-	return img, nil
-}
+	uploadURL, err := storage.PresignPut(
+		s.s3,
+		s.bucket,
+		img.ID,
+	)
 
+	if err != nil {
+		return nil, "", errors.Wrap(err)
+	}
+
+	return img, uploadURL, nil
+}
 func (s *Image) MarkUploaded(
 	ctx context.Context,
 	id string,
