5b98cdfcaedd2356021403ad657a5d2555df1a2b
hakase61912 <hakase61912@icloud.com>
2026-03-16 19:56:03 +0000

    feature: webhook

---
+++ api/internal/webhook/upload.go
@@ -0,0 +1,64 @@
+package webhook
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"sports-day/api/repository"
+
+	"github.com/oklog/ulid/v2"
+)
+
+type S3Event struct {
+	Records []struct {
+		S3 struct {
+			Object struct {
+				Key string `json:"key"`
+			} `json:"object"`
+		} `json:"s3"`
+	} `json:"Records"`
+}
+
+func HandleUploadWebhook(
+	repo repository.Image,
+	secret string,
+	cdnBase string,
+	bucket string,
+) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header.Get("X-Webhook-Secret") != secret {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		var event S3Event
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		for _, record := range event.Records {
+
+			objectKey := record.S3.Object.Key
+			imgID := ulid.MustParse(objectKey)
+
+			publicURL := fmt.Sprintf("%s/%s/%s",
+				cdnBase,
+				bucket,
+				objectKey,
+			)
+
+			_, _ = repo.MarkUploaded(
+				r.Context(),
+				nil,
+				imgID.String(),
+				publicURL,
+			)
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
