4ea68ab1cd2a92dddecfe0b617a67e1ec9ba6c47
Sanshiro Suzuki <kabigon34@icloud.com>
2026-03-17 21:40:06 +0900

    wip

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
   server: {
     host: true,
   },
