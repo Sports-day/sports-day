d90d23c878414c8443524b0da8d244771c0d7b6c
Ishiomaru <spaspaesu@gmail.com>
2026-03-28 21:56:40 +0900

    feat: keycloakをdocker->vmに適用しました

--- demo/authentication-prototpe/src/authentication.ts
+++ demo/authentication-prototpe/src/authentication.ts
@@ -2,11 +2,11 @@
 import { UserManager, WebStorageStateStore, InMemoryWebStorage } from 'oidc-client-ts';
 
 export const userManager = new UserManager({
-  authority: 'http://localhost:8888/realms/sportsday',
+  authority: 'https://dev-idp.sports-day.net/realms/sportsday',
   client_id: 'sportsday',
-  redirect_uri: 'http://localhost:5173/callback',
-  silent_redirect_uri: 'http://localhost:5173/silent-renew.html',
-  post_logout_redirect_uri: 'http://localhost:5173',
+  redirect_uri: 'http://localhost:3000/api/auth/callback',
+  silent_redirect_uri: 'http://localhost:3000/api/auth/silent-callback',
+  post_logout_redirect_uri: 'http://localhost:3000/api/auth/callback',
   response_type: 'code',
   scope: 'openid profile email',
   automaticSilentRenew: true,
